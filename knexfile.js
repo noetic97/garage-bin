c8fdec27e692d690c3fde53c54493d0be07185ec
Joe Heitkamp <noetic97@gmail.com>
2017-09-14 10:08:59 -0600

    Add knexfile, server, and router code

---
+++ knexfile.js
@@ -0,0 +1,38 @@
+module.exports = {
+  
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/garage_bin_dev',
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/dev',
+    },
+    useNullAsDefault: true,
+  },
+  
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/garage_bin_test',
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/test/seeds',
+    },
+    useNullAsDefault: true,
+  },
+  
+  production: {
+    client: 'pg',
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/dev',
+    },
+    useNullAsDefault: true,
+  },
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+exports.db = require('knex')(configuration);
+const router = require('./router');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, './public')));
+app.use('/api/v1', router);
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, './public', 'index.html'));
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Garage Bin is running on ${app.get('port')}.`);
+});
+
+exports.app = app;
