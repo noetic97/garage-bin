5817f6441d5993e167f7028855569a179de26617
Joe Heitkamp <noetic97@gmail.com>
2017-09-14 11:31:38 -0600

    add eslint file and rules

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "extends": ["airbnb", "plugin:react/all"],
+    "plugins": [
+        "react",
+        "import"
+    ],
+    "rules": {
+      "import/prefer-default-export": [0],
+      "arrow-body-style": [0],
+      "react/jsx-filename-extension": [ 1, {"extensions": [".jsx", ".js"] }],
+      "jsx-a11y/href-no-hash": "off",
+      "jsx-a11y/anchor-is-valid": [ 0, { "aspects": ["invalidHref"] }],
+      "react/jsx-no-literals": [ 0, { "noStrings": true }],
+      "react/no-set-state": [0],
+      "react/require-optimization": [0],
+    },
+    "env": {
+      "browser": true,
+      "node": true,
+      "mocha": true,
+    },
+    "globals": {
+      "document": false,
+      "describe": false,
+      "it": false,
+      "beforeEach": false,
+      "should": false
+    },
+    "parserOptions": {
+      "ecmaFeatures": {
+        "jsx": true
+      }
+    }
+};
